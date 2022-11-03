1e627d0ab4c60ee50c7ef59348f113b8e2e50c30
Jesse Lee <jesselee@wirelessprv-10-192-217-89.near.illinois.edu>
2022-11-03 11:12:53 -0500

    fixed tracking files

--- dist/properties/Generated.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IS_GENERATED = void 0;
-exports.IS_GENERATED = {
-    checkbox: true,
-};
--- dist/properties/Status.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SOON_STATUS = void 0;
-exports.SOON_STATUS = {
-    status: {
-        name: "Soon",
-    },
-};
